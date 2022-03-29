8ab4b44e00320729776f2ed96ab3d4e9c41512a2
adrian130695 <adrian_alejandro95@hotmail.com>
2022-03-29 17:25:25 -0600

    ..

--- app/src/main/java/com/example/appmasterdetail/viewmodel/UserAdapterViewModel.kt
+++ app/src/main/java/com/example/appmasterdetail/viewmodel/UserAdapterViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.appmasterdetail.viewmodel
 
+import android.app.Application
 import android.util.Log
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.appmasterdetail.model.ApiService
@@ -11,9 +13,13 @@ import kotlinx.coroutines.launch
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class UserAdapterViewModel : ViewModel() {
+class UserAdapterViewModel(application: Application) : AndroidViewModel(application) {
 
     var userMutableLiveData: MutableLiveData<UserModel> = MutableLiveData()
+
+    init {
+        apiCall()
+    }
 
     fun apiCall() {
         CoroutineScope(Dispatchers.IO).launch {
